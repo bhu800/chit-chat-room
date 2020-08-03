a22b2c8fc72b30cd198db4234bbfcb1bf6ca2855
Bhumanyu Goyal <goyalbhu862001@gmail.com>
2020-08-03 17:04:18 +0530

    added message flasj=hing

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from passlib.hash import pbkdf2_sha256
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 
@@ -44,6 +44,8 @@ def index():
         db.session.add(user)
         db.session.commit()
 
+        flash('Regestired successfully. Please Login!!', category='success')
+
         return redirect(url_for('login'))
 
     return render_template("index.html", form=reg_form)
@@ -68,7 +70,8 @@ def login():
 def chat():
 
     if not current_user.is_authenticated:
-        return "Please login before entring chat!"
+        flash('Please login!', category='danger')
+        return redirect(url_for('login'))
 
     return "Chat with me ;)"
 
@@ -76,7 +79,8 @@ def chat():
 def logout():
 
     logout_user()
-    return "Logged out using flask-login!"
+    flash("You have logged out successfully!", category='success')
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
